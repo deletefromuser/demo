93ea7b8b7b613d6553d815e6fe01aeafae5207fc
deletefromuser <abc@example.com>
2019-03-20 17:23:06 +0900

    standard footer

--- src/main/java/com/hyr/demo/D04_FillFlattenMerge.java
+++ src/main/java/com/hyr/demo/D04_FillFlattenMerge.java
@@ -12,15 +12,16 @@ import java.util.StringTokenizer;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.AcroFields;
 import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.PdfCopy;
 import com.lowagie.text.pdf.PdfImportedPage;
-import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfReader;
-import com.lowagie.text.pdf.PdfSmartCopy;
 import com.lowagie.text.pdf.PdfStamper;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -43,15 +44,23 @@ public class D04_FillFlattenMerge {
 	public void manipulatePdf2(String src, String dest) throws DocumentException, IOException {
 		Document document = new Document();
 		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+
+		BaseFont baseFont = BaseFont.createFont("data/msmincho.ttc,1", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+		HeaderFooter foot = new HeaderFooter(new Phrase("-", new Font(baseFont)), new Phrase("-", new Font(baseFont)));
+		foot.setAlignment(2);
+		document.setFooter(foot);
+
 		document.open();
 		PdfPTable table = new PdfPTable(1);
 		table.setWidthPercentage(100);
 		PdfReader readerheader = new PdfReader("data/header.pdf");
 		PdfImportedPage header = writer.getImportedPage(readerheader, 1);
-		PdfPCell cell = new PdfPCell(Image.getInstance(header));
-		cell.setBorder(PdfPCell.NO_BORDER);
-		cell.setColspan(1);
-		table.addCell(cell);
+//		PdfPCell cell = new PdfPCell(Image.getInstance(header));
+//		cell.setBorder(PdfPCell.NO_BORDER);
+//		cell.setColspan(1);
+//		table.addCell(cell);
+		document.add(Image.getInstance(header));
 
 		ByteArrayOutputStream baos;
 		PdfReader reader;
@@ -77,7 +86,6 @@ public class D04_FillFlattenMerge {
 			fields.setField("timezone2", tokenizer.nextToken());
 			fields.setField("dst", tokenizer.nextToken());
 
-			BaseFont baseFont = BaseFont.createFont("data/msmincho.ttc,1", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
 			fields.addSubstitutionFont(baseFont);
 			fields.setField("name", "Daniel Reuter");
 			fields.setField("sex", "男性");
@@ -88,11 +96,12 @@ public class D04_FillFlattenMerge {
 
 			reader = new PdfReader(baos.toByteArray());
 			PdfImportedPage footer = writer.getImportedPage(reader, 1);
-			cell = new PdfPCell(Image.getInstance(footer));
-			cell.setBorder(PdfPCell.NO_BORDER);
-			cell.setColspan(1);
-			table.addCell(cell);
+//			cell = new PdfPCell(Image.getInstance(footer));
+//			cell.setBorder(PdfPCell.NO_BORDER);
+//			cell.setColspan(1);
+//			table.addCell(cell);
 //			reader.close();
+			document.add(Image.getInstance(footer));
 		}
 		br.close();
 //        reader = new PdfReader("resources/pdfs/footer.pdf");
@@ -100,7 +109,7 @@ public class D04_FillFlattenMerge {
 //        cell = new PdfPCell(Image.getInstance(footer));
 //        cell.setColspan(3);
 //        table.addCell(cell);
-		document.add(table);
+//		document.add(table);
 		document.close();
 	}
 }
