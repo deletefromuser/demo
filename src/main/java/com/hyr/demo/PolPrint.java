758f3db7c58a13a4ce338bc737d3d521741d9810
deletefromuser <abc@example.com>
2019-03-22 12:59:08 +0900

    noon

--- src/main/java/com/hyr/demo/PolPrint.java
+++ src/main/java/com/hyr/demo/PolPrint.java
@@ -7,21 +7,28 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.security.KeyStore.Entry;
+import java.security.KeyStore.TrustedCertificateEntry;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import com.lowagie.text.BadElementException;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
+import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.Image;
 import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.AcroFields;
+import com.lowagie.text.pdf.AcroFields.Item;
 import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.PdfAction;
+import com.lowagie.text.pdf.ColumnText;
 import com.lowagie.text.pdf.PdfContentByte;
-import com.lowagie.text.pdf.PdfFormField;
 import com.lowagie.text.pdf.PdfImportedPage;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfPageEventHelper;
@@ -29,21 +36,19 @@ import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfStamper;
 import com.lowagie.text.pdf.PdfTemplate;
 import com.lowagie.text.pdf.PdfWriter;
-import com.lowagie.text.pdf.PushbuttonField;
 
-public class TextAreaDemo {
+public class PolPrint {
 
-//	public static final String SRC = "resources/pdfs/state.pdf";
+	public static final String HEADER = "data/pol/polHeader.pdf";
+	public static final String POL_TEMP = "data/pol/polFragment.pdf";
 	public static final String SRC = "data/fragment.pdf";
-	public static final String DEST = String.format("target/a-%s.pdf",
-			new SimpleDateFormat("dd-HH-mm-ss").format(new Date()));
-	public static final String DATA = "data/united_states.csv";
+	public static final String DEST = "target/a-result-pol.pdf";
 
 	public static void main(String[] args) throws IOException, DocumentException {
 		System.out.println("--start--");
 		File file = new File(DEST);
 		file.getParentFile().mkdirs();
-		new TextAreaDemo().manipulatePdf2(SRC, DEST);
+		new PolPrint().manipulatePdf2(SRC, DEST);
 		System.out.println("--end--");
 	}
 
@@ -66,34 +71,31 @@ public class TextAreaDemo {
 			PdfContentByte cb = writer.getDirectContent();
 			try {
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				PdfReader reader = new PdfReader("data/textarea.pdf");
+				PdfReader reader = new PdfReader("data/footer.pdf");
 				PdfStamper stamper;
 				AcroFields fields;
 				stamper = new PdfStamper(reader, baos);
 				fields = stamper.getAcroFields();
+				for (Object obj : fields.getFields().entrySet()) {
+					Map.Entry<String, Item> entry = (Map.Entry<String, Item>) obj;
+					// 获得块名
+					String fieldName = entry.getKey();
+					System.out.println(fieldName + ":");
+				}
 				fields.addSubstitutionFont(baseFont);
-				fields.setField("textarea",
-						"保険というライブラリがあります。その中のＯＫＴＨＥＮＬＥＴＵＳＳＥＥｈｈ８９６５８７４５２３６４１２(\"元文字列\", 　を使うと ");
-
+				fields.setField("page.number", "page" + writer.getPageNumber());
+				fields.setField("label", "保険");
+				float[] bs = fields.getFieldPositions("page.count");
 				stamper.setFormFlattening(true);
 				stamper.close();
 				reader.close();
 
 				reader = new PdfReader(baos.toByteArray());
 				PdfImportedPage header = writer.getImportedPage(reader, 1);
-				Image i = Image.getInstance(header);
 				cb.addTemplate(header, 10, 10);
-			} catch (BadElementException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (DocumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				cb.addTemplate(total, bs[1] + 5, bs[4] - 5);
+
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
@@ -109,10 +111,7 @@ public class TextAreaDemo {
 			BaseFont baseFont = null;
 			try {
 				baseFont = BaseFont.createFont("data/msmincho.ttc,1", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-			} catch (DocumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -125,21 +124,81 @@ public class TextAreaDemo {
 	}
 
 	public void manipulatePdf2(String src, String dest) throws DocumentException, IOException {
-		Document document = new Document(PageSize.A4);
+		Document document = new Document(PageSize.A4, 0, 0, 0, 0);
 		printDocProperties(document);
-		document.setJavaScript_onLoad("app.alert('test')");
 		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
-		document.setJavaScript_onLoad("app.alert('test')");
 
 		BaseFont baseFont = BaseFont.createFont("data/msmincho.ttc,1", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+//		HeaderFooter foot = new HeaderFooter(new Phrase("-", new Font(baseFont)), new Phrase("-", new Font(baseFont)));
+//		foot.setAlignment(2);
+//		document.setFooter(foot);
 
 		writer.setPageEvent(new MyFooter());
 		document.open();
-		PdfPTable table = new PdfPTable(1);
-		table.setWidthPercentage(100);
-		PdfReader readerheader = new PdfReader("data/header.pdf");
-		PdfImportedPage header = writer.getImportedPage(readerheader, 1);
+
+		ByteArrayOutputStream baos;
+		PdfReader reader;
+		PdfStamper stamper;
+		AcroFields fields;
+
+		// Header
+		baos = new ByteArrayOutputStream();
+		reader = new PdfReader(HEADER);
+		stamper = new PdfStamper(reader, baos);
+		fields = stamper.getAcroFields();
+		for (Object obj : fields.getFields().entrySet()) {
+			Map.Entry<String, Item> entry = (Map.Entry<String, Item>) obj;
+			// 获得块名
+			String fieldName = entry.getKey();
+			System.out.println(fieldName + ":");
+		}
+		fields.addSubstitutionFont(baseFont);
+		fields.setField("custName", "山田　太郎");
+		fields.setField("birthDate", "１９８０－０１－０２");
+		fields.setField("address", "東京都あきる野市秋川１９８－０１００");
+		fields.setField("tel", "9876543210");
+		stamper.setFormFlattening(true);
+		stamper.close();
+		reader.close();
+
+		reader = new PdfReader(baos.toByteArray());
+		PdfImportedPage header = writer.getImportedPage(reader, 1);
 		document.add(Image.getInstance(header));
+
+		// Fragment
+//		StringTokenizer tokenizer;
+//		BufferedReader br = new BufferedReader(new FileReader(""));
+//		String line = br.readLine();
+//		while ((line = br.readLine()) != null) {
+//			// create a PDF in memory
+//			baos = new ByteArrayOutputStream();
+//			reader = new PdfReader(SRC);
+//			stamper = new PdfStamper(reader, baos);
+//			fields = stamper.getAcroFields();
+//			tokenizer = new StringTokenizer(line, ";");
+//			fields.setField("name", tokenizer.nextToken());
+//			fields.setField("abbr", tokenizer.nextToken());
+//			fields.setField("capital", tokenizer.nextToken());
+//			fields.setField("city", tokenizer.nextToken());
+//			fields.setField("population", tokenizer.nextToken());
+//			fields.setField("surface", tokenizer.nextToken());
+//			fields.setField("timezone1", tokenizer.nextToken());
+//			fields.setField("timezone2", tokenizer.nextToken());
+//			fields.setField("dst", tokenizer.nextToken());
+//
+//			fields.addSubstitutionFont(baseFont);
+//			fields.setField("name", "Daniel Reuter");
+//			fields.setField("sex", "男性");
+//			fields.setField("age", "35歳");
+//			stamper.setFormFlattening(true);
+//			stamper.close();
+//			reader.close();
+//
+//			reader = new PdfReader(baos.toByteArray());
+//			PdfImportedPage footer = writer.getImportedPage(reader, 1);
+//			document.add(Image.getInstance(footer));
+//		}
+//		br.close();
 
 		document.close();
 	}
